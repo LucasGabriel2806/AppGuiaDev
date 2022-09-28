5fb70328e477207c10598ecc0dee2d956986e89e
Lucas Gabriel <pintolucas996@gmail.com>
2022-09-28 15:57:50 -0300

    Primeiro Commit

---
+++ AppGuiaDev/AppGuiaDev/Views/ComponentesPrimeiro.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using AppGuiaDev.Models;
+
+namespace AppGuiaDev.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ComponentesPrimeiro : ContentPage
+    {
+        public ComponentesPrimeiro()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componente
+                {
+                    Nome = "Linguagem, Trabalho e Tecnologia",
+
+                    AtribuicoesResponsabilidades = "Comunicar-se em língua portuguesa, utilizando o vocabulário técnico da área e " +
+                                                   "elaborar registros e planilhas de acompanhamentto e controle de atividades",
+
+                    ValoresAtitudes = "Incentivar o diálogo e a interlocução. º Responsabilizar-se pela produção, utilização e " +
+                                      "divulgação de informações. º Estimular a proatividade. ",
+                };
+
+                await Navigation.PushAsync(new VerComponente(c));
+            }
+            catch
+            {
+                await DisplayAlert("Ops!", ex.Mesasge, "OK");/////////////////////////////
+            }
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_4(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_5(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_6(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_7(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
